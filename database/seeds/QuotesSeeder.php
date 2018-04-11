05c3eb26216ecf20d6e70de2c0f35e32ca3abcb3
Lee Pownall <leepownall@gmail.com>
2018-04-11 19:43:15 +0100

    Update seeder

--- database/seeds/QuotesSeeder.php
+++ database/seeds/QuotesSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Quote;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use League\Csv\Reader;
 use League\Csv\Statement;
 
@@ -14,7 +15,27 @@ class QuotesSeeder extends Seeder
         $records = (new Statement())->process($reader);
 
         foreach ($records as $record) {
-            Quote::create($record);
+            Quote::create([
+                'body' => array_get($record, 'body'),
+                'created_at' => $this->createdAtIsNowIfIncorrect(array_get($record, 'created_at')),
+                'deleted_at' => $this->deleteIfLowPoints(array_get($record, 'points'))
+            ]);
         }
+    }
+
+    private function createdAtIsNowIfIncorrect(string $createdAt): string
+    {
+        return $createdAt === '0000-00-00 00:00:00'
+            ? Carbon::now()->format('Y-m-d h:i:s')
+            : $createdAt;
+    }
+
+    private function deleteIfLowPoints(string $points): ?string
+    {
+        if ((int) $points < 0) {
+            return Carbon::now()->format('Y-m-d h:i:s');
+        }
+
+        return null;
     }
 }
