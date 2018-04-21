6c169cb7792908bde969b2524df9db7cdcf6f140
Lee Pownall <leepownall@gmail.com>
2018-04-21 22:48:47 +0100

    Rename requests

---
+++ app/Http/Requests/StoreQuoteRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\MoreThanSomeSay;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreQuoteRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'quote' => [
+                'required',
+                new MoreThanSomeSay,
+                'max:280',
+                'unique:quotes,body',
+            ]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'quote.required' => 'A quote is required.'
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $quote = trim($this->quote);
+
+        if (str_contains($quote, ['Some say', 'some say', 'Some Say']) === false) {
+            $quote = 'Some say ' . $quote;
+        }
+
+        $this->merge(['quote' => $quote]);
+    }
+}
