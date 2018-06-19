a0b127b74a00b4907aafd62844ea462c39e8b9d3
Lee Pownall <leepownall@gmail.com>
2018-06-19 10:59:51 +0100

    Add links

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,13 @@
   <div class="container">
     <ul class="list-unstyled list-inline text-small mb-0">
       <li class="list-inline-item">
+        <a href="https://twitter.com/stigquotes" class="text-white" target="_blank">@stigquotes</a>
+      </li>
+      <li class="list-inline-item">
         <a href="https://github.com/leepownall/stig-quotes" class="text-white" target="_blank">GitHub</a>
+      </li>
+      <li class="list-inline-item float-sm-right">
+        <a href="https://www.alexmills-website.co.uk/" class="text-white" target="_blank">Hosted by www.alexmills-website.co.uk</a>
       </li>
     </ul>
   </div>
